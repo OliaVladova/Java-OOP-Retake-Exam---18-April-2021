15fa275c153ef9cb798e399aa38789cd9183195f
OliaVladova <ovladova1@gmail.com>
2022-06-19 22:36:53 +0300

    Add files via upload

---
+++ SpaceStation/models/mission/MissionImpl.java
@@ -0,0 +1,45 @@
+package spaceStation.models.mission;
+
+import spaceStation.models.astronauts.Astronaut;
+import spaceStation.models.planets.Planet;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+
+public class MissionImpl implements Mission{
+    private int deadAstronauts;
+
+    @Override
+    public void explore(Planet planet, Collection<Astronaut> astronauts) {
+
+        List<Astronaut> astronautList = new ArrayList<>(astronauts);
+        List<String> planetItems = new ArrayList<>(planet.getItems());
+
+        for (int ast = 0; ast < astronautList.size(); ast++) {
+            Astronaut astronaut = astronautList.get(ast);
+
+            for (int itm = 0; itm < planetItems.size(); itm++) {
+                String item = planetItems.get(itm);
+
+                astronaut.breath();
+                astronaut.getBag().getItems().add(item);
+                planetItems.remove(itm);
+
+                itm--;
+
+                if (!astronaut.canBreath()) {
+                    this.deadAstronauts++;
+                    break;
+                }
+            }
+        }
+    }
+
+    @Override
+    public int getDeadAstronauts() {
+        return this.deadAstronauts;
+    }
+
+}
